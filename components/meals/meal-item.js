eb3f9fa18799d5a66a3c9d3796b51624fefad4f8
Dev Modi <devmodi910@gmail.com>
2025-02-24 14:31:34 +0530

    all default pages tried

--- components/meals/meal-item.js
+++ components/meals/meal-item.js
@@ -9,7 +9,7 @@ export default function MealItem({ title, slug, image, summary, creator }) {
       <header>
         <div className={classes.image}>
           <Image
-            src={`https://maxschwarzmueller-nextjs-demo-users-image.s3.amazonaws.com/${image}`}
+            src={{image}}
             alt={title}
             fill
           />
